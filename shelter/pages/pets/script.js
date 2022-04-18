9619b2c2705bd76a0c2f91ba16aae497d8f9bca2
Yura Lapitski <lyurik@tut.by>
2022-04-18 21:49:22 +0300

    feat: edit footer

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -30,8 +30,8 @@ fotoBloks.forEach((el) => (el.style.background = `url("${fotoPets[i++]}")`));
 i = 0;
 nameBloks.forEach((el) => (el.innerText = namePets[i++]));
 
-$(document).ready(function () {
-  $(".hamburger").click(function () {
-    $(this).toggleClass("is-active");
-  });
-});
+// $(document).ready(function () {
+//   $(".hamburger").click(function () {
+//     $(this).toggleClass("is-active");
+//   });
+// });
