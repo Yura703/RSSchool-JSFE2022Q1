960d1ec39ed4687764bcf9ca44f06985195cc726
Yura Lapitski <lyurik@tut.by>
2022-04-21 22:01:49 +0300

    feat: finished slider for main

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -118,34 +118,82 @@ const fotoPets = [
   "../../assets/pets/scarlett.png",
   "../../assets/pets/freddie.png",
 ];
-let i = 0;
 
+let numberCards = getRandomeNumberCards([]);
 let fotoBloks = document.querySelectorAll("div.card__foto");
-
 let nameBloks = document.querySelectorAll("div.card > p");
 
-fotoBloks.forEach((el) => (el.style.background = `url("${fotoPets[i++]}")`));
-
-i = 0;
-nameBloks.forEach((el) => (el.innerText = namePets[i++]));
-
-//получение случайного числа, за исключением некоторых чисел
-function getRandonNamber(missNambers) {
-  while (true) {
-    const number = getRandomNumberBetween(0, 7);
-    if (missNambers.indexOf(number) === -1) {
-      return number;
-    }
-  }
+for (let i = 0; i < 3; i++) {
+  fotoBloks[3 + i].style.background = `url("${fotoPets[numberCards[i]]}")`;
+  nameBloks[3 + i].innerText = namePets[numberCards[i]];
 }
 
 //карусель
 const BTN_LEFT = document.querySelector(".btn-left");
 const BTN_RIGHT = document.querySelector(".btn-right");
+const SLIDER = document.querySelector("#slider");
+const ITEM_LEFT = document.querySelector("#item-left");
+const ITEM_RIGHT = document.querySelector("#item-right");
+const LEFT = true;
+const RIGHT = false;
 
-BTN_LEFT.addEventListener("click", () => {});
+//запуск анимации
+const moveLeft = () => {
+  numberCards = getRandomeNumberCards(numberCards);
+  initCard(numberCards, true);
+  SLIDER.classList.add("transition-left");
+  BTN_LEFT.removeEventListener("click", moveLeft);
+  BTN_RIGHT.removeEventListener("click", moveRight);
+};
 
-//вспомогательные функции
-function getRandomNumberBetween(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+const moveRight = () => {
+  numberCards = getRandomeNumberCards(numberCards);
+  initCard(numberCards, false);
+  SLIDER.classList.add("transition-right");
+  BTN_LEFT.removeEventListener("click", moveLeft);
+  BTN_RIGHT.removeEventListener("click", moveRight);
+};
+
+BTN_LEFT.addEventListener("click", moveLeft);
+BTN_RIGHT.addEventListener("click", moveRight);
+
+//действия после завершения анимации
+SLIDER.addEventListener("animationend", (animationEvent) => {
+  let changedItem;
+  if (animationEvent.animationName === "move-left") {
+    SLIDER.classList.remove("transition-left");
+    changedItem = ITEM_LEFT;
+    document.querySelector("#item-active").innerHTML = ITEM_LEFT.innerHTML;
+  } else {
+    SLIDER.classList.remove("transition-right");
+    changedItem = ITEM_RIGHT;
+    document.querySelector("#item-active").innerHTML = ITEM_RIGHT.innerHTML;
+  }
+
+  BTN_LEFT.addEventListener("click", moveLeft);
+  BTN_RIGHT.addEventListener("click", moveRight);
+});
+
+function getRandomeNumberCards(cardNambers) {
+  let newCardsNumber = [];
+  while (true) {
+    const number = Math.floor(Math.random() * 7);
+    if (
+      cardNambers.indexOf(number) === -1 &&
+      newCardsNumber.indexOf(number) === -1
+    ) {
+      newCardsNumber.push(number);
+    }
+    if (newCardsNumber.length === 3) {
+      return newCardsNumber;
+    }
+  }
+}
+
+function initCard(numbers, direction) {
+  const index = direction === true ? 0 : 6;
+  for (let i = 0; i < 3; i++) {
+    fotoBloks[index + i].style.background = `url("${fotoPets[numbers[i]]}")`;
+    nameBloks[index + i].innerText = namePets[numbers[i]];
+  }
 }
