a8e2b3632c587a07b7e4ef74224dc74f430199ba
Yura Lapitski <lyurik@tut.by>
2022-06-20 23:46:46 +0300

    feat: add tsconfig.json and extension .eslint.json

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '22a4dc610b144c49860b8bc3dad51bb8', // получите свой ключ https://newsapi.org/
         });
     }
 }
