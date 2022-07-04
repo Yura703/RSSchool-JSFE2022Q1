cb7fae6b4f50fd04a4db66a9103a5d27f589e05e
Yura Lapitski <lyurik@tut.by>
2022-07-04 22:15:26 +0300

    fix: link edit to https://nodenews.herokuapp.com/

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,8 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: '22a4dc610b144c49860b8bc3dad51bb8', // получите свой ключ https://newsapi.org/
+        super('https://nodenews.herokuapp.com/', {
+            apiKey: '22a4dc610b144c49860b8bc3dad51bb8',  // получите свой ключ https://newsapi.org/
         });
     }
 }
